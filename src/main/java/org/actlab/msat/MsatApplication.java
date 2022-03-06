ae1634b4fd70c16ac88f916f2633fe7c7f1065a8
guredora <contact@guredora.com>
2022-03-06 22:09:41 +0900

    initial commit

---
+++ src/main/java/org/actlab/msat/MsatApplication.java
@@ -0,0 +1,13 @@
+package org.actlab.msat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/actlab/msat/MsatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.actlab.msat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
