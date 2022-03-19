d5ebf0005f0d55009cb2b2ba7714013d415cab5d
guredora <contact@guredora.com>
2022-03-19 20:47:01 +0900

    add Searchsettinginterface

---
+++ src/main/java/org/actlab/msat/core/searchsetting/Searchsettinginterface.java
@@ -0,0 +1,7 @@
+package org.actlab.msat.core.searchsetting;
+
+import org.actlab.msat.core.settingobjects.Setting;
+
+public interface Searchsettinginterface {
+    public boolean search(Setting setting);
+}
