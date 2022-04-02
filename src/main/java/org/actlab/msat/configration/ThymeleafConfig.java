51957ca937a0292624ee497bf700e16d08004b2f
guredora <contact@guredora.com>
2022-04-02 19:29:31 +0900

    thymeleafのtemplateEngineを自分で生成するように変更 プレーンテキスト用のtemplateEngineが利用可能になった

---
+++ src/main/java/org/actlab/msat/configration/ThymeleafConfig.java
@@ -0,0 +1,38 @@
+package org.actlab.msat.configration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+    @Primary // ⑩
+    @Bean
+    public SpringTemplateEngine pageTemplateEngine() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setTemplateMode( TemplateMode.HTML );
+        resolver.setPrefix( "templates/" );
+        resolver.setSuffix( ".html" );
+        resolver.setCharacterEncoding( "UTF-8" );
+        resolver.setCheckExistence(true);
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver( resolver );
+        return engine;
+    }
+
+    @Bean("messageTemplateEngine") // ⑧
+    public SpringTemplateEngine messageTemplateEngine() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setTemplateMode( TemplateMode.TEXT );
+        resolver.setPrefix( "configfiles/" );
+        resolver.setSuffix( ".txt");
+        resolver.setCharacterEncoding( "UTF-8" );
+        resolver.setCacheable( true );
+        var engine = new SpringTemplateEngine();
+        engine.setTemplateResolver( resolver );
+        return engine;
+    }    
+}
